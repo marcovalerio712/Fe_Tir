945e8419fa9a0aa00d7740e7fe70dd230aac0ef6
Wibbley <marcovalerio71299@gmail.com>
2023-10-02 12:39:34 +0200

    Spostato App Components nella cartella component

--- prog-tir-frontend/src/main.ts
+++ prog-tir-frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './components/app/app.module';
+import { AppModule } from './component/app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
