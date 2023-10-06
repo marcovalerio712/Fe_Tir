b31b181e4edd5b93089f183d722b9081035c111a
MariusOlteanu98 <olteanumarius1998@gmail.com>
2023-10-06 14:14:38 +0200

    Registrazione Dipendente

--- prog-tir-frontend/src/components/app/registrazione/registrazione.component.ts
+++ prog-tir-frontend/src/components/app/registrazione/registrazione.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Dipendente } from 'src/models/dipendente';
 import { DipendenteService } from 'src/service/DipendenteService/dipendente.service';
 
 @Component({
@@ -8,16 +9,14 @@ import { DipendenteService } from 'src/service/DipendenteService/dipendente.serv
   styleUrls: ['./registrazione.component.css']
 })
 export class RegistrazioneComponent {
+  registerUrl: string = 'https://cdn-icons-png.flaticon.com/512/6521/6521996.png';
+  dipendente: Dipendente = {}; // Inizializza l'oggetto dipendente vuoto
+listaRuoli: any;
 
-registerUrl: string ='https://cdn-icons-png.flaticon.com/512/6521/6521996.png';
-
-constructor(private dipendenteService: DipendenteService,private router: Router) {}
+  constructor(private dipendenteService: DipendenteService, private router: Router) {}
 
   onSubmit() {
-    const credentials = {
-    };
-
-    this.dipendenteService.registerDipendente(credentials).subscribe(response => {
+    this.dipendenteService.registerDipendente(this.dipendente).subscribe(response => {
       console.log('Risposta dal server:', response);
     });
   }
